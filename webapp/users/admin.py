bf181e0635dd7f1b7687ddeb6eb47b9c5f9759ef
josemicode <jmiguelojedagutierrez@gmail.com>
2025-04-07 12:58:46 +0200

    added vote model

--- webapp/users/admin.py
+++ webapp/users/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from users import models
 
 admin.site.register(models.User)
+admin.site.register(models.Vote)
--- webapp/users/models.py
+++ webapp/users/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+#? Reimplement, add CuOOra context
 class User(AbstractUser):
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
      
+class Vote(models.Model):
+    is_positive_vote = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #...
+    def is_like(self):
+        return self.is_positive_vote
+    
+    def get_user(self):
+        pass #* First, assign foreign key
+
+    def like(self):
+        self.is_positive_vote = True
+
+    def dislike(self):
+        self.is_positive_vote = False
